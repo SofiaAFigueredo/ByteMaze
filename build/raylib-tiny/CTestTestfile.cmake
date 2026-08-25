d037bcaed430abf497cbae5141a6d30b14454fb6
SofiaAFigueredo <sofiaalvesfigueredo@gmail.com>
2026-08-24 21:29:28 -0300

    Revert "feat: implementar prototipo jogavel com labirinto procedural, jogador rotacionavel e build otimizada para o limite de 1.44MB"
    This reverts commit f9373ecd4f8beadcad61561454f585f9bd7f3f27.

--- build/raylib-tiny/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davi/Documentos/GitHub/ByteMaze/raylib
-# Build directory: /home/davi/Documentos/GitHub/ByteMaze/build/raylib-tiny
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- build/raylib-tiny/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davi/Documentos/GitHub/ByteMaze/raylib/src
-# Build directory: /home/davi/Documentos/GitHub/ByteMaze/build/raylib-tiny/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
